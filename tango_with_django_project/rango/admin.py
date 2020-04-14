41901b408ab784becc706fc39f34758f8e7f7aea
rmccreath <r.mccreath@nhs.net>
2020-04-14 21:54:44 +0100

    ch6.3

--- tango_with_django_project/rango/admin.py
+++ tango_with_django_project/rango/admin.py
@@ -6,6 +6,9 @@ class PageAdmin(admin.ModelAdmin):
     list_display = ('title', 'category', 'url')
 
 
-admin.site.register(Category)
-admin.site.register(Page, PageAdmin)
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
 
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Page, PageAdmin)
